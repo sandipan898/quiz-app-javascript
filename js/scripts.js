cf7996da9dc8bcca4c36703ff547acb3989bc289
sandipan <sandipan@localhost.localdomain>
2020-06-25 01:56:05 -0700

    update styling and functions

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function submitAnswers() {
+    let total = 5;
+    let score = 0;
+
+    // Get the user input 
+    let q1 = document.forms['quizForm']['q1'].value;
+    let q2 = document.forms['quizForm']['q2'].value;
+    let q3 = document.forms['quizForm']['q3'].value;
+    let q4 = document.forms['quizForm']['q4'].value;
+    let q5 = document.forms['quizForm']['q5'].value;
+
+    // Validation
+}
