71b1782e562a898fb2ec0492c283a3bc14b9e9fe
sandipan <sandipan@localhost.localdomain>
2020-06-25 02:56:12 -0700

    update functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,32 @@ function submitAnswers() {
 
     // Get the user input 
     let q1 = document.forms['quizForm']['q1'].value;
+    // console.log(document.forms['quizForm']);
     let q2 = document.forms['quizForm']['q2'].value;
     let q3 = document.forms['quizForm']['q3'].value;
     let q4 = document.forms['quizForm']['q4'].value;
     let q5 = document.forms['quizForm']['q5'].value;
+    // alert(q1);
 
     // Validation
+
+    for(i = 1; i<=total; i++) {
+        if(eval('q'+i) == null || eval('q'+i) == "") {
+            alert('Ops! You missed question ' + i);
+            return false;
+        }
+    }
+
+    // Set Correct Answers
+    var answers = ['b', 'a', 'd', 'b', 'd'];
+
+    // Check the Answers
+    for(i = 1; i<=total; i++) {
+        if(eval('q'+i) == answers[i-1]) {
+            score++;
+        }
+    }
+
+    // Display result
+    alert("You scored " + score + " out of " + total);
 }
